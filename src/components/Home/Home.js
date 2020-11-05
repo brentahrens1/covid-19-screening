ad258b07898df7669bfadb209a9bd7d0cb5bd3a3
Brent Ahrens <cidida@gmail.com>
2020-11-05 14:17:07 -0800

    home form work in progress

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,6 +9,7 @@ import logo from '../../assets/images/logo.png'
 
 const Home = () => {
     const [ isLoaded, setIsLoaded ] = useState(false)
+    const [ isActive, setIsActive ] = useState(false)
     
     useEffect(() => {
         setTimeout(() => {
@@ -30,7 +31,7 @@ const Home = () => {
                             <form>
                                 <input type="text" />
                                 <p>Employee Name</p>
-                                <select value="department">
+                                <select>
                                     <option>IT</option>
                                     <option>Reception</option>
                                     <option>Clinician</option>
@@ -38,25 +39,25 @@ const Home = () => {
                                 <p>Department Name</p>
                                 <p>1. Have you had a fever of 100.4 degrees or higher or a new or worsening cough/ shortness of breath/ sore throat or body aches?</p>
                                 <div className="checkbox-container">
-                                    <div className="checkboxes">
+                                    <label className="checkboxes" htmlFor="yes">Yes
                                         <input className="checkbox-input" type="checkbox" name="yes" />
-                                        <label htmlFor="yes">Yes</label>
-                                    </div>
-                                    <div className="checkboxes">
-                                        <input className="checkbox-input" type="checkbox" name="no" />
-                                        <label htmlFor="no">No</label>
-                                    </div>
+                                        <span></span>
+                                    </label>
+                                    <label className="checkboxes" htmlFor="yes">Yes
+                                        <input className="checkbox-input" type="checkbox" name="yes" />
+                                        <span></span>
+                                    </label>
                                 </div>
                                 <p>2. Have you had close contact (6 fett or less and for 15 minutes or more) with a known Covid-19 patient?</p>
                                 <div className="checkbox-container">
-                                    <div className="checkboxes">
+                                <label className="checkboxes" htmlFor="yes">Yes
                                         <input className="checkbox-input" type="checkbox" name="yes" />
-                                        <label htmlFor="yes">Yes</label>
-                                    </div>
-                                    <div className="checkboxes">
-                                        <input className="checkbox-input" type="checkbox" name="no" />
-                                        <label htmlFor="no">No</label>
-                                    </div>
+                                        <span></span>
+                                    </label>
+                                    <label className="checkboxes" htmlFor="yes">Yes
+                                        <input className="checkbox-input" type="checkbox" name="yes" />
+                                        <span></span>
+                                    </label>
                                 </div>
                                 <p>If your response is yes to either question please notify your supervisor immediatley.</p>
                                 <p>Seek a doctors advice in addition to use this App before making any medical decisions.</p>
