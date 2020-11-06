a1f9547334a704a27de7cb31399efa8afe7bf65e
Brent Ahrens <cidida@gmail.com>
2020-11-05 19:57:24 -0800

    adds new styles to form and adds active classes to checkboxes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -10,6 +10,10 @@ import logo from '../../assets/images/logo.png'
 const Home = () => {
     const [ isLoaded, setIsLoaded ] = useState(false)
     const [ isSubmitted, setIsSubmitted ] = useState(false)
+    const [ isChecked1, setIsChecked1 ] = useState(false)
+    const [ isChecked2, setIsChecked2 ] = useState(false)
+    const [ isChecked3, setIsChecked3 ] = useState(false)
+    const [ isChecked4, setIsChecked4 ] = useState(false)
 
     const handleSubmit = () => {
         setIsSubmitted(!isSubmitted)
@@ -59,26 +63,26 @@ const Home = () => {
                                     </select>
                                     <p className="input-p">Department Name</p>
                                     <p>1. Have you had a fever of 100.4 degrees or higher or a new or worsening cough/ shortness of breath/ sore throat or body aches?</p>
-                                    <div className="checkbox-container">
-                                        <label className="checkboxes" htmlFor="yes">Yes
-                                            <input className="checkbox-input" type="checkbox" name="yes" />
-                                            <span></span>
-                                        </label>
-                                        <label className="checkboxes" htmlFor="yes">Yes
-                                            <input className="checkbox-input" type="checkbox" name="yes" />
-                                            <span></span>
-                                        </label>
+                                    <div className="checkboxes">
+                                        <div className="checkboxes__inner">
+                                            <input className={`${isChecked1 ? "active" : ""}`} onClick={() => setIsChecked1(!isChecked1)} type="checkbox" name="yes" />
+                                            <label className="checkboxes" htmlFor="yes">Yes</label>
+                                        </div>
+                                        <div className="checkboxes__inner">
+                                            <input className={`${isChecked2 ? "active" : ""}`} onClick={() => setIsChecked2(!isChecked2)} type="checkbox" name="no" />
+                                            <label className="checkboxes" htmlFor="no">No</label>
+                                        </div>
                                     </div>
                                     <p>2. Have you had close contact (6 fett or less and for 15 minutes or more) with a known Covid-19 patient?</p>
-                                    <div className="checkbox-container">
-                                    <label className="checkboxes" htmlFor="yes">Yes
-                                            <input className="checkbox-input" type="checkbox" name="yes" />
-                                            <span></span>
-                                        </label>
-                                        <label className="checkboxes" htmlFor="yes">Yes
-                                            <input className="checkbox-input" type="checkbox" name="yes" />
-                                            <span></span>
-                                        </label>
+                                    <div className="checkboxes">
+                                        <div className="checkboxes__inner">
+                                            <input className={`${isChecked3 ? "active" : ""}`} onClick={() => setIsChecked3(!isChecked3)} type="checkbox" name="yes" />
+                                            <label className="checkboxes" htmlFor="yes">Yes</label>
+                                        </div>
+                                        <div className="checkboxes__inner">
+                                            <input className={`${isChecked4 ? "active" : ""}`} onClick={() => setIsChecked4(!isChecked4)} type="checkbox" name="no" />
+                                            <label className="checkboxes" htmlFor="no">No</label>
+                                        </div>
                                     </div>
                                     <p>If your response is yes to either question please notify your supervisor immediatley.</p>
                                     <p>Seek a doctors advice in addition to use this App before making any medical decisions.</p>
