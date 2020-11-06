4106b706f1534d7ad73dfedc56cfd71f1c000f32
Brent Ahrens <cidida@gmail.com>
2020-11-05 16:44:33 -0800

    adds desktop styles to the form

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -28,59 +28,63 @@ const Home = () => {
                 (
                     <div className="home">
                         <div className="home__form">
-                            <div className="logo">
-                                <img src={logo} alt="Company logo goes here" />
+                            <div className="logo-container">
+                                <div className="logo">
+                                    <img src={logo} alt="Company logo goes here" />
+                                </div>
                             </div>
-                            {
-                                isSubmitted ? 
-                                (
-                                    <>
-                                    <h1>Thank you</h1>
-                                    <p>Your Covid-19 health screening has been submited to your manager. Have a great day</p>
-                                    </>
-                                )
-                                :
-                                (   
-                                    <>
-                                    <h1>Covid-19 Health Screening</h1>
-                                    </>
-                                )
-                            }
-                            <form className={`${isSubmitted ? "hide" : ""}`}>
-                                <input type="text" />
-                                <p>Employee Name</p>
-                                <select>
-                                    <option>IT</option>
-                                    <option>Reception</option>
-                                    <option>Clinician</option>
-                                </select>
-                                <p>Department Name</p>
-                                <p>1. Have you had a fever of 100.4 degrees or higher or a new or worsening cough/ shortness of breath/ sore throat or body aches?</p>
-                                <div className="checkbox-container">
+                            <div>
+                                {
+                                    isSubmitted ? 
+                                    (
+                                        <>
+                                        <h1>Thank you</h1>
+                                        <p>Your Covid-19 health screening has been submited to your manager. Have a great day</p>
+                                        </>
+                                    )
+                                    :
+                                    (   
+                                        <>
+                                        <h1>Covid-19 Health Screening</h1>
+                                        </>
+                                    )
+                                }
+                                <form className={`${isSubmitted ? "hide" : ""}`}>
+                                    <input type="text" />
+                                    <p className="input-p">Employee Name</p>
+                                    <select>
+                                        <option>IT</option>
+                                        <option>Reception</option>
+                                        <option>Clinician</option>
+                                    </select>
+                                    <p className="input-p">Department Name</p>
+                                    <p>1. Have you had a fever of 100.4 degrees or higher or a new or worsening cough/ shortness of breath/ sore throat or body aches?</p>
+                                    <div className="checkbox-container">
+                                        <label className="checkboxes" htmlFor="yes">Yes
+                                            <input className="checkbox-input" type="checkbox" name="yes" />
+                                            <span></span>
+                                        </label>
+                                        <label className="checkboxes" htmlFor="yes">Yes
+                                            <input className="checkbox-input" type="checkbox" name="yes" />
+                                            <span></span>
+                                        </label>
+                                    </div>
+                                    <p>2. Have you had close contact (6 fett or less and for 15 minutes or more) with a known Covid-19 patient?</p>
+                                    <div className="checkbox-container">
                                     <label className="checkboxes" htmlFor="yes">Yes
-                                        <input className="checkbox-input" type="checkbox" name="yes" />
-                                        <span></span>
-                                    </label>
-                                    <label className="checkboxes" htmlFor="yes">Yes
-                                        <input className="checkbox-input" type="checkbox" name="yes" />
-                                        <span></span>
-                                    </label>
-                                </div>
-                                <p>2. Have you had close contact (6 fett or less and for 15 minutes or more) with a known Covid-19 patient?</p>
-                                <div className="checkbox-container">
-                                <label className="checkboxes" htmlFor="yes">Yes
-                                        <input className="checkbox-input" type="checkbox" name="yes" />
-                                        <span></span>
-                                    </label>
-                                    <label className="checkboxes" htmlFor="yes">Yes
-                                        <input className="checkbox-input" type="checkbox" name="yes" />
-                                        <span></span>
-                                    </label>
-                                </div>
-                                <p>If your response is yes to either question please notify your supervisor immediatley.</p>
-                                <p>Seek a doctors advice in addition to use this App before making any medical decisions.</p>
-                                <button onClick={handleSubmit}>Submit</button>
-                            </form>
+                                            <input className="checkbox-input" type="checkbox" name="yes" />
+                                            <span></span>
+                                        </label>
+                                        <label className="checkboxes" htmlFor="yes">Yes
+                                            <input className="checkbox-input" type="checkbox" name="yes" />
+                                            <span></span>
+                                        </label>
+                                    </div>
+                                    <p>If your response is yes to either question please notify your supervisor immediatley.</p>
+                                    <p>Seek a doctors advice in addition to use this App before making any medical decisions.</p>
+                                    <button onClick={handleSubmit}>Submit</button>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 )
