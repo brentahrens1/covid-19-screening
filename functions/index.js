2d4c12f3751c1ae212d1ff0baa715615049e4182
Brent Ahrens <cidida@gmail.com>
2020-12-20 16:19:11 -0800

    adds cc if yes

--- functions/index.js
+++ functions/index.js
@@ -16,19 +16,22 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 app.post('/api/screening', async (req,res) => {
-    console.log('hit')
     console.log(JSON.parse(req.body))
     const { name, department, selectedOption, selectedOption2 } = JSON.parse(req.body)
     const msg = {
         to: 'brentholmesahrens@gmail.com',
         from: 'cidida@gmail.com',
         subject: `COVID-19 Screen Results for ${name}: ${
-            selectedOption !== 'yes' && selectedOption2 !== 'yes' ? "ALL CLEAR!" : "Warning"
+            selectedOption === 'yes' || selectedOption2 === 'yes' ? "Warning" : "All Clear"
         }`,
         text:
         `Department: ${department}
         1. Have you had a fever of 100.4 degrees or higher or a new or worsening cough/ shortness of breath/ sore throat or body aches? ${selectedOption}
         2. Have you had close contact (6 fett or less and for 15 minutes or more) with a known Covid-19 patient? ${selectedOption2}`
+    }
+    if (selectedOption === 'yes' || selectedOption2 === 'yes') {
+        msg.cc = 'cidida@gmail.com'
+        console.log(msg)
     }
     try {
         const mailRes = await sgMail.send(msg)
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -11,7 +11,6 @@ import logo from '../../assets/images/logo.png'
 import { Link, useHistory } from 'react-router-dom'
 
 const Home = () => {
-    const [ isLoaded, setIsLoaded ] = useState(false)
     const [ screeningFormData, setScreeningFormData ] = useState({})
     const [ selectedOption, setSelectedOption ] = useState('')
     const [ selectedOption2, setSelectedOption2 ] = useState('')
@@ -46,98 +45,81 @@ const Home = () => {
     const handleOptionChange2 = (e) => {
         setSelectedOption2(e.target.value)
     }
-    
-    useEffect(() => {
-        setTimeout(() => {
-            setIsLoaded(true)
-        }, 3000)
-    }, [])
 
     return (
         <div className="home">
-            {
-                isLoaded ? 
-                (
-                    <div className="home">
-                        <div className="home__form">
-                            <div className="logo-container">
-                                <div className="logo">
-                                    <img src={logo} alt="Company logo goes here" />
-                                </div>
-                            </div>
-                            <div>
-                                <h1>Covid-19 Health Screening</h1>
-                                <form>
-                                    <input name="name" type="text" value={screeningFormData.name || ''} onChange={handleChange} required />
-                                    <p className="input-p">Employee Name</p>
-                                    <select name="department" onChange={handleChange} required>
-                                        <option></option>
-                                        <option value="reception">Reception</option>
-                                        <option value="clinician">Clinician</option>
-                                    </select>
-                                    <p className="input-p">Department (Manager)</p>
-                                    <p>1. Have you had a fever of 100.4 degrees or higher or a new or worsening cough/ shortness of breath/ sore throat or body aches?</p>
-                                    <div className="checkbox-container">
-                                        <div className="checkbox">
-                                            <label htmlFor="yes">Yes</label>
-                                            <input 
-                                                type="radio" 
-                                                name="question-one" 
-                                                value="yes" 
-                                                checked={selectedOption === 'yes'}
-                                                onChange={handleOptionChange} 
-                                            />
-                                        </div>
-                                        <div className="checkbox">
-                                            <label htmlFor="no">No</label>
-                                            <input
-                                                type="radio" 
-                                                name="question-one" 
-                                                value="no"
-                                                checked={selectedOption === 'no'}
-                                                onChange={handleOptionChange}
-                                            />
-                                        </div>
-                                    </div>
-                                    <p>2. Have you had close contact (6 fett or less and for 15 minutes or more) with a known Covid-19 patient?</p>
-                                    <div className="checkbox-container">
-
-                                        <div className="checkbox">
-                                            <label htmlFor="yes">Yes</label>
-                                            <input 
-                                                type="radio" 
-                                                name="question-two" 
-                                                value="yes" 
-                                                checked={selectedOption2 === 'yes'}
-                                                onChange={handleOptionChange2} 
-                                            />
-                                        </div>
-                                        <div className="checkbox">
-                                            <label htmlFor="no">No</label>
-                                            <input
-                                                type="radio" 
-                                                name="question-two" 
-                                                value="no"
-                                                checked={selectedOption2 === 'no'}
-                                                onChange={handleOptionChange2}
-                                            />
-                                        </div>
-                                    </div>
-                                    <p>If your response is yes to either question please notify your supervisor immediatley.</p>
-                                    <p>Seek a doctors advice in addition to use this App before making any medical decisions.</p>
-                                    <button onClick={handleSubmit}>Submit</button>
-                                </form>
-                            </div>
+            <div className="home">
+                <div className="home__form">
+                    <div className="logo-container">
+                        <div className="logo">
+                            <img src={logo} alt="Company logo goes here" />
                         </div>
                     </div>
-                )
-                :
-                (
-                    <>
-                    <LoadScreen />
-                    </>
-                )
-            }
+                    <div>
+                        <h1>Covid-19 Health Screening</h1>
+                        <form>
+                            <input name="name" type="text" value={screeningFormData.name || ''} onChange={handleChange} required />
+                            <p className="input-p">Employee Name</p>
+                            <select name="department" onChange={handleChange} required>
+                                <option></option>
+                                <option value="reception">Reception</option>
+                                <option value="clinician">Clinician</option>
+                            </select>
+                            <p className="input-p">Department (Manager)</p>
+                            <p>1. Have you had a fever of 100.4 degrees or higher or a new or worsening cough/ shortness of breath/ sore throat or body aches?</p>
+                            <div className="checkbox-container">
+                                <div className="checkbox">
+                                    <label htmlFor="yes">Yes</label>
+                                    <input 
+                                        type="radio" 
+                                        name="question-one" 
+                                        value="yes" 
+                                        checked={selectedOption === 'yes'}
+                                        onChange={handleOptionChange} 
+                                    />
+                                </div>
+                                <div className="checkbox">
+                                    <label htmlFor="no">No</label>
+                                    <input
+                                        type="radio" 
+                                        name="question-one" 
+                                        value="no"
+                                        checked={selectedOption === 'no'}
+                                        onChange={handleOptionChange}
+                                    />
+                                </div>
+                            </div>
+                            <p>2. Have you had close contact (6 fett or less and for 15 minutes or more) with a known Covid-19 patient?</p>
+                            <div className="checkbox-container">
+
+                                <div className="checkbox">
+                                    <label htmlFor="yes">Yes</label>
+                                    <input 
+                                        type="radio" 
+                                        name="question-two" 
+                                        value="yes" 
+                                        checked={selectedOption2 === 'yes'}
+                                        onChange={handleOptionChange2} 
+                                    />
+                                </div>
+                                <div className="checkbox">
+                                    <label htmlFor="no">No</label>
+                                    <input
+                                        type="radio" 
+                                        name="question-two" 
+                                        value="no"
+                                        checked={selectedOption2 === 'no'}
+                                        onChange={handleOptionChange2}
+                                    />
+                                </div>
+                            </div>
+                            <p>If your response is yes to either question please notify your supervisor immediatley.</p>
+                            <p>Seek a doctors advice in addition to use this App before making any medical decisions.</p>
+                            <button onClick={handleSubmit}>Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
