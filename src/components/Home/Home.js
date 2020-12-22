de92fda74d1b75216f1b8b800154c93736e50b79
Brent Ahrens <cidida@gmail.com>
2020-12-21 16:20:58 -0800

    adds btn disabled if form not filled out

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -115,7 +115,7 @@ const Home = () => {
                             </div>
                             <p>If your response is yes to either question please notify your supervisor immediatley.</p>
                             <p>Seek a doctors advice in addition to use this App before making any medical decisions.</p>
-                            <button onClick={handleSubmit}>Submit</button>
+                            <button disabled={screeningFormData === {}, selectedOption === '', selectedOption2 === ''} onClick={handleSubmit}>Submit</button>
                         </form>
                     </div>
                 </div>
